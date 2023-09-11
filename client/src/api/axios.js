cc43e3200b54828538e0f15bbe3f8f137121e823
gnanachandra <vvit.20bq1a05p2@gmail.com>
2023-09-11 12:47:37 +0530

    backend url added

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 export default axios.create({
-    baseURL : "http://localhost:5000"
+    baseURL : "https://fantastic-polo-shirt-ant.cyclic.app"
 })
-//"https://s2win-394715.uc.r.appspot.com"
+//""
